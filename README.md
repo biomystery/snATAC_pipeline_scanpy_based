334f62c8c3bc0dca38dd2740caed90ce10f052b0
frank@tscc <zhangc518@gmail.com>
2019-07-19 10:51:39 -0700

    added frop/totalreads scatter for single sample

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 snATAC pipeline 
+==========
+
+** Steps 
+
+1. Run 10x cellranger 
+2. Run preprocessing pipeline 
+3. Run `Run_prefilter.py` to get thresholds 
+4. Run `Run_jupyter.py` to do initial clustering
+5. Run `Run_scrublet.py` to get scrublet doublet score
+6. Run `Run_merge.py` to merge samples 
+
+** Improvements 
+
+1. Get `cut_freq_matrix` for prom x cells 
+2. Group frag/reads for (cells in) clusters, then call peaks 
+3. Merge frag/reads 
+4. 
+
 
--- Run_jupyter.py
+++ Run_jupyter.py
@@ -8,15 +8,18 @@ import sys
 import subprocess
 
 sample_name = sys.argv[1]
-threshold = sys.argv[2]
+min_usable_reads = sys.argv[2]
+min_frop = sys.argv[3]
 print("Running jupyter:" + sample_name)
 pm.execute_notebook(
     'clustering_single.ipynb',
     '{0}/{0}.ipynb'.format(sample_name),
     parameters={"sample_name": sample_name,
-                "min_usable_reads": threshold
+                "min_usable_reads": min_usable_reads,
+                "min_frop": min_frop,
                 }
 )
 
-subprocess.call(["jupyter-nbconvert", "--to", "html",'{0}/{0}.ipynb'.format(sample_name)])
+subprocess.call(["jupyter-nbconvert", "--to", "html",
+                 '{0}/{0}.ipynb'.format(sample_name)])
 # In[ ]:
