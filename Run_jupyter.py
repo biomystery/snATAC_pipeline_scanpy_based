909c556d5acc8bb6f3f9a2661ec7ab9fd6c30419
frank@tscc <zhangc518@gmail.com>
2019-07-19 12:09:33 -0700

    added report number of cells in filter output

--- Run_jupyter.py
+++ Run_jupyter.py
@@ -6,17 +6,21 @@
 import papermill as pm
 import sys
 import subprocess
+import pandas as pd
 
 sample_name = sys.argv[1]
-min_usable_reads = sys.argv[2]
-min_frop = sys.argv[3]
+
+# Read thresholds from file
+ths = pd.read_csv('{0}/{0}.qc_thresholds.txt'.format(sample_name), sep=',')
+print(ths)
+
 print("Running jupyter:" + sample_name)
 pm.execute_notebook(
     'clustering_single.ipynb',
     '{0}/{0}.ipynb'.format(sample_name),
     parameters={"sample_name": sample_name,
-                "min_usable_reads": min_usable_reads,
-                "min_frop": min_frop,
+                "min_usable_reads": str(ths['min_total'][0]),
+                "min_frop": str(ths['min_FRoP'][0]),
                 }
 )
 
